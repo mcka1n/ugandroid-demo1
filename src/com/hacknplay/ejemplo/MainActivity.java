5f217604c55b07fe8f2f5e31f382db904aac26b5
Ed Mckain <mckain.default@gmail.com>
2013-11-15 22:52:26 -0600

    add Android demo1 project files.

---
+++ src/com/hacknplay/ejemplo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.hacknplay.ejemplo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	Button btnSearch;
+	Button btnOpenActivity;
+	public static final String TAG = MainActivity.class.toString();
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // Initialize buttons (applying cast)
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        btnOpenActivity = (Button) findViewById(R.id.btnOpenActivity);
+        // Specify to the buttons which Listener is gonna be ready for them.
+        ButtonListener listener = new ButtonListener();
+        btnSearch.setOnClickListener(listener);
+        btnOpenActivity.setOnClickListener(listener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    // Listener 
+    class ButtonListener implements OnClickListener {
+    	@Override
+    	
+    	public void onClick(View v){
+    		Log.i(TAG, "Hizo click!!");
+    		Toast.makeText(getApplicationContext(), "OMG Hizo Click!!", Toast.LENGTH_SHORT).show();
+    	}
+    }
+     
+}
