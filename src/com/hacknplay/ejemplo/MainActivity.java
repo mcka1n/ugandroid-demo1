a0f17be1a3d3245783507e3d37108ff4e628e4ed
Ed Mckain <mckain.default@gmail.com>
2013-12-01 12:05:36 -0600

    complete demo2 video requirements.

--- src/com/hacknplay/ejemplo/MainActivity.java
+++ src/com/hacknplay/ejemplo/MainActivity.java
@@ -1,12 +1,15 @@
 package com.hacknplay.ejemplo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -41,8 +44,23 @@ public class MainActivity extends Activity {
     	@Override
     	
     	public void onClick(View v){
-    		Log.i(TAG, "Hizo click!!");
-    		Toast.makeText(getApplicationContext(), "OMG Hizo Click!!", Toast.LENGTH_SHORT).show();
+    		//Log.i(TAG, "Hizo click!!");
+    		//Toast.makeText(getApplicationContext(), "OMG Hizo Click!!", Toast.LENGTH_SHORT).show();
+    		EditText searchQuery = (EditText)findViewById(R.id.editTextSearchQuery);
+    		String searchQueryText = searchQuery.getText().toString();
+    		String url = "https://www.google.com/?q=" + searchQueryText + "#q=" + searchQueryText;
+    		Intent intent = null;
+    		if (v.getId() == btnOpenActivity.getId()) {
+    			intent = new Intent(getApplicationContext(), 
+						ShowSearchQueryActivity.class);
+    			intent.putExtra(ShowSearchQueryActivity.QUERY, searchQueryText);
+    		} else if (v.getId() == btnSearch.getId()) {
+    			intent = new Intent(Intent.ACTION_VIEW);
+    			intent.setData(Uri.parse(url));
+    		}
+    		
+    		startActivity(intent);
+    		
     	}
     }
      
--- src/com/hacknplay/ejemplo/ShowSearchQueryActivity.java
+++ src/com/hacknplay/ejemplo/ShowSearchQueryActivity.java
@@ -2,14 +2,23 @@ package com.hacknplay.ejemplo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class ShowSearchQueryActivity extends Activity {
-
+	public static final String QUERY = "query";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_search_query);
+		
+		Intent intent = getIntent();
+		String queryText = intent.getStringExtra(QUERY);
+		if (queryText != null){
+			TextView txtMsg = (TextView)findViewById(R.id.txtMsg);
+			txtMsg.setText(queryText);
+		}
 	}
 
 	@Override
